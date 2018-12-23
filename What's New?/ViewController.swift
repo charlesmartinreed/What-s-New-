93576aabeb01ea8d0ede48d15aff594545e1b4c7
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-12-22 23:49:26 -0600

    Build dummy example using static data and default configuration.

--- What's New?/ViewController.swift
+++ What's New?/ViewController.swift
@@ -8,13 +8,47 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //called here because we cannot present another view controller during the viewDidLoad lifecycle
+        whatsNewIfNeeded()
+    }
+    
+    func whatsNewIfNeeded() {
+        let items = [
+            WhatsNew.Item(
+                title: "A New America",
+                subtitle: "Making America Great, for Everyone, for the first time.",
+                image: UIImage(named: "feature1")),
+            WhatsNew.Item(
+                title: "A New America",
+                subtitle: "Making America Great, for Everyone, for the first time.",
+                image: UIImage(named: "feature2")),
+            WhatsNew.Item(
+                title: "A New America",
+                subtitle: "Making America Great, for Everyone, for the first time.",
+                image: UIImage(named: "feature3")),
+            WhatsNew.Item(
+                title: "A New America",
+                subtitle: "Making America Great, for Everyone, for the first time.",
+                image: UIImage(named: "feature4"))
+            ]
+        
+        //using a predefined style
+        let whatsNew = WhatsNew(title: "2020", items: items)
+        let whatsNewVC = WhatsNewViewController(whatsNew: whatsNew)
+        
+        //present the view controller
+        present(whatsNewVC, animated: true, completion: nil)
+    }
 }
 
