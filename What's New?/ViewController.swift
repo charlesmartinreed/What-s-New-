8c5223ee88d4f0b39fb9b561004b29479a607991
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-12-22 23:11:36 -0600

    Initial Commit

---
+++ What's New?/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  What's New?
+//
+//  Created by Charles Martin Reed on 12/22/18.
+//  Copyright © 2018 Charles Martin Reed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
